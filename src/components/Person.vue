70c5d4b580137e6c80f56942bbb0bb394dde8e5f
Yiang <1712064922@qq.com>
2023-12-20 20:48:08 +0800

    setup() 语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,43 +2,42 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{ address }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  name: 'components_Person',
-  setup() {
-    // 数据，弱化了 this
-    let name = '张三'
-    let age = 12
-    // 此时并非响应式数据
-    let tel = '123465789'
-
-    // 方法
-    function changeName() {
-      if (name === 'zhangSan') {
-        name = '张三'
-      } else {
-        name = 'zhangSan'
-      }
-    }
-    function changeAge() {
-      age += 1
-    }
-    function showTel() {
-      alert(tel)
-    }
-    return { name, age, tel, changeName, changeAge, showTel }
-    // setup() 也可以返回渲染函数
-    // return () => '哈哈'
-  }
+  name: 'components_Person'
 }
 </script>
+<script lang="ts" setup>
+// 数据
+let name = '张三'
+let age = 12
+let tel = '123465789'
+let address = '监狱'
 
+// 方法
+function changeName() {
+  if (name === 'zhangSan') {
+    name = '张三'
+  } else {
+    name = 'zhangSan'
+  }
+}
+
+function changeAge() {
+  age += 1
+}
+
+function showTel() {
+  alert(tel)
+}
+</script>
 <style scoped>
 .person {
   background-color: cyan;
