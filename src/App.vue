1b21fa8267c6cfbf8eca68f4ea61b68d0b133317
Yiang <1712064922@qq.com>
2023-12-20 21:23:05 +0800

    reactive对象类型响应式数据

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <components_Person />
+  <hr />
+  <components_Car />
 </template>
 
 <script>
 import components_Person from '@/components/Person.vue'
+import components_Car from '@/components/Car.vue'
 export default {
   // 组件名
   name: 'App',
-  components: { components_Person }
+  components: { components_Person, components_Car }
 }
 </script>
 
---
+++ src/components/Car.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="car">
+    <h2>一辆{{ car.brand }}车，价值{{ car.price }}万美金</h2>
+    <button @click="changePrice()">修改价格</button>
+    <hr />
+    <h2>游戏列表</h2>
+    <ul>
+      <li v-for="game in games" v-bind:key="game.id">{{ game.name }}</li>
+    </ul>
+    <button @click="changeGameName()">修改游戏名称</button>
+    <hr />
+    <h2>测试：{{ obj.a.b.c }}</h2>
+    <button @click="changeObj()">修改测试对象</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'components_Car'
+}
+</script>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+let car = reactive({ brand: '奔驰', price: 100 })
+let games = reactive([
+  { id: 'game1', name: '农' },
+  { id: 'game2', name: '舟' },
+  { id: 'game3', name: '原' }
+])
+let obj = reactive({
+  a: {
+    b: {
+      c: 54
+    }
+  }
+})
+function changePrice() {
+  car.price += 10
+}
+function changeGameName() {
+  games[0].name = '王者荣耀'
+  games[1].name = '明日方舟'
+  games[2].name = '原神启动'
+}
+function changeObj() {
+  obj.a.b.c = 666
+}
+</script>
+<style scoped>
+.car {
+  background-color: bisque;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 0 5px;
+}
+li {
+  font-size: 20px;
+}
+</style>
