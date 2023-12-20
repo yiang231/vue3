bab2290cc38683e1daef449124edab1991008460
Yiang <1712064922@qq.com>
2023-12-20 22:00:33 +0800

    reactive 和 ref

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="car">
-    <h2>一辆{{ car.brand }}车，价值{{ car.price }}万美金</h2>
+    <h2>一辆{{ car1.brand }}车，价值{{ car1.price }}万美金</h2>
+    <h2>两辆{{ car2.brand }}车，价值{{ car2.price }}万美金</h2>
     <button @click="changePrice()">修改价格</button>
+    <button @click="changeBrand()">修改品牌</button>
+    <button @click="changeCar1()">修改车子1</button>
+    <button @click="changeCar2()">修改车子2</button>
     <hr />
-    <h2>游戏列表</h2>
-    <ul>
-      <li v-for="game in games" v-bind:key="game.id">{{ game.name }}</li>
-    </ul>
-    <button @click="changeGameName()">修改游戏名称</button>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum()">修改求和</button>
   </div>
 </template>
 
@@ -17,20 +18,26 @@ export default {
 }
 </script>
 <script lang="ts" setup>
-import { ref } from 'vue'
-let car = ref({ brand: '奔驰', price: 100 })
-let games = ref([
-  { id: 'game1', name: '农' },
-  { id: 'game2', name: '舟' },
-  { id: 'game3', name: '原' }
-])
+import { ref, reactive } from 'vue'
+let car1 = ref({ brand: '奔驰', price: 100 })
+let car2 = reactive({ brand: '奔驰', price: 100 })
+let sum = ref(0)
 function changePrice() {
-  car.value.price += 10
+  car1.value.price += 10
 }
-function changeGameName() {
-  games.value[0].name = '王者荣耀'
-  games.value[1].name = '明日方舟'
-  games.value[2].name = '原神启动'
+function changeBrand() {
+  car1.value.brand = '问界'
+}
+function changeCar1() {
+  // 替换 ref 对象
+  car1.value = { brand: '劳斯莱斯', price: 3000 }
+}
+function changeCar2() {
+  // 替换 reactive 对象，将除了第一个对象外的所有对象赋值给第一个对象
+  Object.assign(car2, { brand: '劳斯莱斯', price: 5000 })
+}
+function changeSum() {
+  sum.value += 1
 }
 </script>
 <style scoped>
