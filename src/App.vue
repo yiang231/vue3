872132599eec52a6a0b56e485acfc4a3d946f3d8
Yiang <1712064922@qq.com>
2023-12-23 23:26:44 +0800

    标签的 ref 属性

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,29 @@
     <components_Watch />
     <hr />
     <components_WatchEffect />
+    <hr />
+    <components_RefAttr ref="refAttr" />
+    <br />
+    <button @click="showLog">展示refAttr</button>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-<script>
+let refAttr = ref()
+
+function showLog() {
+    // 获取的是组件实例对象
+    console.log(refAttr.value)
+}
+</script>
+<script lang="ts">
 import components_Person from '@/components/Person.vue'
 import components_Car from '@/components/Car.vue'
 import components_Book from '@/components/Book.vue'
 import components_Computed from '@/components/Computed.vue'
 import components_Watch from '@/components/Watch.vue'
 import components_WatchEffect from '@/components/WatchEffect.vue'
+import components_RefAttr from '@/components/RefAttr.vue'
 
 export default {
     // 组件名
@@ -29,9 +43,14 @@ export default {
         components_Book,
         components_Computed,
         components_Watch,
-        components_WatchEffect
+        components_WatchEffect,
+        components_RefAttr
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+button {
+    margin: 0 5px;
+}
+</style>
---
+++ src/components/RefAttr.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="refAttr">
+        <h1>省</h1>
+        <h2 ref="t2">市</h2>
+        <h3>区</h3>
+        <button @click="showLog">输出 H2 元素</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'components_RefAttr'
+}
+</script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+// 创建一个 t2 ，用于存储 ref 标记的内容
+let t2 = ref()
+
+let a = ref('a')
+let b = ref('b')
+let c = ref('c')
+
+function showLog() {
+    // <h2 data-v-41aeb112="">市</h2> 获取的是 DOM 元素
+    // data-v-41aeb112="" 是局部样式 <style scoped>
+    console.log(t2.value)
+}
+// 定义组件实例对象需要暴露的内容
+defineExpose({ a, c })
+</script>
+<style scoped>
+.refAttr {
+    background-color: aliceblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+</style>
