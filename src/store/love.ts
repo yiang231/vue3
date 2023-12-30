76ca1fab1fe56fb51c2876644b2d78edda81683e
Yiang <1712064922@qq.com>
2023-12-30 21:17:35 +0800

    store组合式Api

--- src/store/love.ts
+++ src/store/love.ts
@@ -1,22 +1,20 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 import { nanoid } from 'nanoid'
+import { reactive } from 'vue'
 
-export const useLoveStore = defineStore('love', {
-    actions: {
-        async getTalk() {
-            const url = 'https://api.uomg.com/api/rand.qinghua?format=json'
-            // 结果解构再解构赋值，加重命名
-            const {
-                data: { content: title }
-            } = await axios.get(url)
+export const useLoveStore = defineStore('love', () => {
+    const talkList = reactive(JSON.parse(localStorage.getItem('talkList') as string) || [])
 
-            this.talkList.unshift({ id: nanoid(), title })
-        }
-    },
-    state() {
-        return {
-            talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
-        }
+    async function getTalk() {
+        const url = 'https://api.uomg.com/api/rand.qinghua?format=json'
+        // 结果解构再解构赋值，加重命名
+        const {
+            data: { content: title }
+        } = await axios.get(url)
+
+        talkList.unshift({ id: nanoid(), title })
     }
+
+    return { talkList, getTalk }
 })
