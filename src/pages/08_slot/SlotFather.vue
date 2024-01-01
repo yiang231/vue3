ef7aa1c96a49ff94e6d633b77cb5b888f42918bc
Yiang <1712064922@qq.com>
2024-01-01 15:07:14 +0800

    组件通信_8_作用域slot

--- src/pages/08_slot/SlotFather.vue
+++ src/pages/08_slot/SlotFather.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="father">
         <h3>父组件</h3>
+        <h4>具名插槽</h4>
         <div class="content">
             <Category>
                 <template v-slot:s2>
@@ -29,11 +30,38 @@
                 </template>
             </Category>
         </div>
+        <hr />
+        <h4>（具名）作用域插槽</h4>
+        <!--压岁钱案例-->
+        <div class="content">
+            <Game>
+                <template v-slot:default="{ games }">
+                    <ul>
+                        <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+                    </ul>
+                </template>
+            </Game>
+            <Game>
+                <template v-slot:default="{ games, x }">
+                    <ol>
+                        <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+                        <li>{{ x }}</li>
+                    </ol>
+                </template>
+            </Game>
+            <Game>
+                <template #default="{ games, y }">
+                    <h3 v-for="game in games" :key="game.id">{{ game.name }}</h3>
+                    <h3>{{ y }}</h3>
+                </template>
+            </Game>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import Category from './SlotCategory.vue'
+import Game from './SlotGame.vue'
 import { ref, reactive } from 'vue'
 import { nanoid } from 'nanoid'
 
@@ -64,7 +92,8 @@ video {
     width: 100%;
 }
 
-h2 {
+h2,
+h4 {
     background-color: orange;
     text-align: center;
     font-size: 20px;
---
+++ src/pages/08_slot/SlotGame.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="game">
+        <h2>游戏列表</h2>
+        <slot name="default" :games="games" x="嘿嘿" y="哈哈"></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { nanoid } from 'nanoid'
+
+let games = reactive([
+    { id: nanoid, name: '英雄联盟' },
+    { id: nanoid, name: '王者农药' },
+    { id: nanoid, name: '红色警戒' },
+    { id: nanoid, name: '斗罗大陆' }
+])
+</script>
+
+<style scoped>
+.game {
+    width: 200px;
+    height: 300px;
+    background-color: skyblue;
+    border-radius: 10px;
+    box-shadow: 0 0 10px;
+}
+
+h2 {
+    background-color: orange;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 800;
+}
+</style>
